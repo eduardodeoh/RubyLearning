f8010aa42aab388be693d8d3b9feb799fbc2c7cf
Eduardo de O. Hernandes <eduardo@eduardo-ubuntu.(none)>
2012-02-05 22:44:22 -0200

    Start Week2

--- Week1/p005methods.rb
+++
@@ -1,8 +0,0 @@
-#Metodos gets (get string) e chomp
-
-puts "Digite sua cidade: "
-STDOUT.flush
-cidade = gets.chomp
-#cidade = gets
-puts "A cidade... = " + cidade
-puts "...terminou"
--- Week1/p007dt.rb
+++
@@ -1,7 +0,0 @@
-#Tipagem dinamica em Ruby
-x = 7
-x = "house"
-x = 7.5
-
-#Tudo em Ruby é um objeto
-"I love Ruby".length
